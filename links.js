b317c90375280fb02d39d42e44e0dd7a360e8971
yesid <euspiritus@gmail.com>
2019-02-25 18:31:14 -0500

    Nuevos archivos 2

---
+++ links.js
@@ -0,0 +1,48 @@
+
+    /* ---- Javascript ---- */
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Aqui el resultado campeon";
+// }
+
+
+// Corfirmacion de respuesta del enlaze "Running" // 
+
+function preguntar() {
+    var respuesta = confirm("Deseas ir a Running?")
+    return respuesta;
+} 
+function preguntar2() {
+    var respuesta = confirm("Deseas ir a Training?")
+    return respuesta;
+}
+function preguntar3() {
+    var respuesta = confirm("Deseas ir a Urbano?")
+    return respuesta;
+}
+
+function preguntar4() {
+    var respuesta = confirm("Deseas ir a Running?")
+    return respuesta;
+}
+function preguntar5() {
+    var respuesta = confirm("Deseas ir a Training?")
+    return respuesta;
+}
+function preguntar6() {
+    var respuesta = confirm("Deseas ir a Urbano?")
+    return respuesta;
+}
+function preguntar7() {
+    var respuesta = confirm("Deseas ir a Running?")
+    return respuesta;
+}
+
+function preguntar8() {
+    var respuesta = confirm("Deseas ir a Training?")
+    return respuesta;
+}
+
+function preguntar9() {
+    var respuesta = confirm("Deseas ir a Urbano?")
+    return respuesta;
+}
